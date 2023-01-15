e5f334172aa138166e4dd1d69268cf849755870e
Ubskiy <ArsenAvakov1@yandex.ru>
2023-01-15 12:26:25 +0300

    Initial Commit

---
+++ Giphy/Models/GiphyAPIModel.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// Сетевая модель для получения и декодинга гифок
+struct GiphyAPIModel: Decodable {
+    // Данные для гифки
+    let data: Data?
+}
+
+extension GiphyAPIModel {
+    struct Data: Decodable {
+        // Уникальный идентификатор гифки
+        let id: String?
+        // Ссылка для скачивания гифки
+        let images: Images?
+    }
+}
+
+extension GiphyAPIModel.Data {
+    struct Images: Decodable {
+        let preview_gif: PreviewGif?
+    }
+}
+
+extension GiphyAPIModel.Data.Images {
+    struct PreviewGif: Decodable {
+        let url: URL?
+    }
+}
